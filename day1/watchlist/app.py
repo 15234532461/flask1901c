2b7b0386223632d13cb81ad06c3420432d354937
JJH <15234532461>
2020-04-02 17:26:48 +0800

    模板继承,模板上下文！

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,22 +1,92 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-@app.route('/')
+import os
+import sys
 
+from flask import Flask,render_template
+import click
+from flask_sqlalchemy import SQLAlchemy  #导入扩展类
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭了对模型修改的监控
+db = SQLAlchemy(app)  #初始化扩展,传入程序实例app
+#models
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+# 模板上下文处理函数
+@app.context_processor
+def common_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+# views
+@app.route('/')
 def index():
+    
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',movies=movies)
+# #动态URL
+# @app.route('/index/<name>')
+# def home(name):
+#     #print(url_for("home",name="queen"))
+#     return "<h1>Hello,%s</h1>"%name
+
+#自定义命令
+# 新建data.db的数据库初始化命令
+@app.cli.command()  #这个装饰器，可以注册命令
+@click.option('--drop',is_flag=True,help="删除后再创建")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成")
+
+#向data.db中写入数据的命令
+@app.cli.command()
+def forge():
     name = "Queen"
     movies = [
         {"title":"大赢家","year":"2020"},
         {"title":"七龙珠","year":"2019"},
         {"title":"海贼王","year":"2018"},
         {"title":"火影忍者","year":"2017"},
-        {"title":"速度与激情","year":"2019"},
+        {"title":"追龙","year":"2008"},
+        {"title":"上海堡垒","year":"2019"},
+        {"title":"大话西游","year":"2017"},
+        {"title":"超人归来","year":"2016"},
+        {"title":"囧妈","year":"2012"},
     ]
-    return render_template('index.html',name=name,movies=movies)
-# #动态URL
-# @app.route('/index/<name>')
-# def home(name):
-#     #print(url_for("home",name="queen"))
-#     return "<h1>Hello,%s</h1>"%name
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo("插入数据完成！")
+
+
+#错误处理函数
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    #返回模板和状态码
+    return render_template('404.html'),404
+
 
 
 if __name__ == '__main__':

9e444c7115e2bf9bb41dee79602ad6b68581673e
JJH <15234532461>
2020-04-03 16:04:08 +0800

    表单完结

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -57,7 +57,7 @@ def index():
 # def home(name):
 #     #print(url_for("home",name="queen"))
 #     return "<h1>Hello,%s</h1>"%name
-
+#更新电影信息
 @app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
@@ -74,6 +74,19 @@ def edit(movie_id):
         flash('更新完成')
         return redirect(url_for('index'))
     return render_template('edit.html',movie=movie)
+
+#删除电影信息
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除成功！')
+    return redirect(url_for('index'))
+
+
+
+
 
 #自定义命令
 # 新建data.db的数据库初始化命令

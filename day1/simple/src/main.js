8f6cbb9b9be96fa75caf722ae737e0aa5867aee4
JJH <15234532461>
2020-04-15 17:44:21 +0800

    index.vue完成

--- day1/simple/src/main.js
+++ day1/simple/src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Base from './Base.vue'
+import VueRouter from 'vue-router'
+import IndexPage from './components/index.vue'
 // import HelloComponent from './HelloComponent.vue'
 
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes:[{
+      path:'/',
+      component:IndexPage
+    }]
+})
 new Vue({
   el: '#app',
+  router,
   components:{
     Base,
     // HelloComponent
   },
   template:'<Base/>'
-
 })

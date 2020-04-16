530f2668cf3aab0bdabd8ca0cac7112aa06caae2
JJH <15234532461>
2020-04-16 18:07:35 +0800

    轮播图

--- day1/simple/src/components/index.vue
+++ day1/simple/src/components/index.vue
@@ -1,242 +1,196 @@
 <template>
-    <div class="index-wrapper" >
-        <div class="index-left" >
-            <div class="index-left-block all-products">
-                <h2>全部产品</h2>
-                <template v-for="product in productList" >
-                    <h3>{{ product.title }}</h3>
-                <ul>
-                    <li v-for="item in product.list">
-                        <a v-bind:href="item.url">{{ item.title }}</a>
-                        <span class="hot-tag" v-if="item.hot" >HOT</span>
-                    </li>
-                </ul>
-                <hr v-if="!product.list" />
-                </template>
-            </div>
-            <template>
-                <div class="index-left-block lastest-news">
-                <h2>最新消息</h2>
-                <ul>
-                    <li v-for="xiaoxis in newsList">
-                        <a v-bind:href="xiaoxis.url">{{ xiaoxis.title }}</a>
-                    </li>
-                </ul>
-                </div>
-            </template>
-            
-        </div>
-        <div class="index-right" >
-            <div style="font-size:40px;text-align:center;line-height:300px;width:900px;background: #4fc08d;" >
-                slider
-            </div>
-            <div class="index-board-list">
-                <div class="index-board-item" >
-                    <div class="index-board-item-inner">
-                    <h2>联想电脑</h2>
-                    <p>联想电脑就是牛</p>
-                    <div class="index-board-button" >立即购买</div>
-                    </div>
-                </div>
-                <div class="index-board-item" >
-                    <div class="index-board-item-inner">
-                    <h2>联想电脑</h2>
-                    <p>联想电脑就是牛</p>
-                    <div class="index-board-button" >立即购买</div>
-                    </div>
-                </div>
-                <div class="index-board-item" >
-                    <div class="index-board-item-inner">
-                    <h2>联想电脑</h2>
-                    <p>联想电脑就是牛</p>
-                    <div class="index-board-button" >立即购买</div>
-                    </div>
-                </div>
-                <div class="index-board-item" >
-                    <div class="index-board-item-inner">
-                    <h2>联想电脑</h2>
-                    <p>联想电脑就是牛</p>
-                    <div class="index-board-button" >立即购买</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="index-wrapper">
+    <div class="index-left">
+      <div class="index-left-block all-products">
+        <h2>全部产品</h2>
+        <template v-for="product in productList">
+          <h3>{{ product.title }}</h3>
+          <ul>
+            <li v-for="item in product.list">
+              <a v-bind:href="item.url">{{ item.title }}</a>
+              <span v-if="item.hot" class="hot-tag">HOT</span>
+            </li>
+          </ul>
+          <hr v-if="!product.last" />
+        </template>
+      </div>
+      <div class="index-left-block lastest-news">
+        <h2>最新消息</h2>
+        <ul>
+          <li v-for="news in newsList">
+            <a v-bind:href="news.url">{{ news.title }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</template>>
+    <div class="index-right">
+      <slider-component></slider-component>
+      <div class="index-board-list">
+        <div class="index-board-item" v-for="item in boardList">
+          <div class="index-board-item-inner">
+            <h2>{{ item.title }}</h2>
+            <p>{{ item.description }}</p>
+            <div class="index-board-button">立即购买</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 
 <script>
 import axios from "axios";
+import SliderComponent from './sliderComponent'
 export default {
-    mounted() {
-        var that=this
-        axios.get("/getNewsList")
-          .then((response) => {
-              //headle success
-              console.log(response);
-              that.newsList = response.data.list
-          })
-          .catch((error) => {
-              // handle error
-              console.log(error);
-          });
-    },
-    data() {
-        return {
-            newsList:[
-                
-            ],
-            // xiaoxi:{
-            //         name:[
-            //             {
-            //                 content: "手机不是终点，汽车才是华为的下一个目标",
-            //                 url: "https://tech.sina.com.cn/mobile/n/n/2020-04-13/doc-iircuyvh7041870.shtml"
-            //             },
-            //             {
-            //                 content: "屡次突破拍照极限，华为P40带来手机摄影更多想象",
-            //                 url: "https://baijiahao.baidu.com/s?id=1663850916373477627&wfr=spider&for=pc"
-            //             },
-            //             {
-            //                 content: "联想刘军：新财年国内PC份额涨2% 服务营收破70亿元",
-            //                 url: "http://finance.sina.com.cn/stock/relnews/hk/2020-04-15/doc-iircuyvh7903952.shtml"
-            //             },
-            //             {
-            //                 content: "最新消息：苹果将今晚发布iPhone9(SE2)!",
-            //                 url: "http://news.ifeng.com/c/7vgvg1USoTg"
-            //             },
-            //             {
-            //                 content: "售价惊人的外星人Aurora台式电脑将来，你会买吗？",
-            //                 url: "https://baijiahao.baidu.com/s?id=1612316559468102586&wfr=spider&for=pc"
-            //             },
-            //         ]
-            //     },
-            productList:{
-                pc: {
-                    title:"PC产品",
-                    list:[
-                        {
-                            title:"数据统计",
-                            url:"http://starcraft.com"
-                        },
-                        {
-                            title: "数据预测",
-                            url: "http://warcraft.com",
-                            hot:true,
-                        },
-                        {
-                            title: "流量分析",
-                            url: "http://overwatch.com",
-                            hot: true
-                        },
-                        {
-                            title: "广告发布",
-                            url: "http://hearstone.com"
-                        }
-                    ]
-                },
-                app: {
-                    title:"手机应用类",
-                    last:true,
-                    list:[
-                        {
-                          title: "91助手",
-                          url: "http://weixin.com"
-                        },
-                        {
-                          title: "产品助手",
-                          url: "http://weixin.com",
-                          hot:true,
-                        },
-                        {
-                          title: "智能地图",
-                          url: "http://maps.com",
-                        },
-                        {
-                          title: "语音助手",
-                          url: "http://phone.com",
-                          hot:true,
-                        }
-                    ]
-                }
-            }
-        }
-    },
-}
+  components:{
+    SliderComponent
+  },
+  mounted() {
+    axios.get("api/getNewsList")
+      .then((response) => {
+        // handle success
+        console.log(response);
+        this.newsList = response.data.list
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+      axios.get("api/getProductList")
+      .then((response) => {
+        // handle success
+        console.log(response);
+        this.productList = response.data
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+      axios.get("api/getBoardList")
+      .then((response) => {
+        // handle success
+        console.log(response);
+        this.boardList = response.data
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  },
+  data() {
+    return {
+      newsList: [],
+      productList: null,
+      boardList:null
+    };
+  }
+};
 </script>
-<style>
-   .index-wrapper{
-       width: 1200px;
-       margin:0 auto;
-       display: flex;
-   }
-    .index-left{
-        width: 300px;
 
-    }
-    .index-right{
-        width: 900px;
-    }
-    .index-left-block{
-        margin: 15px;
-        background: white;
-        box-shadow: 0 0 1px #ddd;
-        border-radius: 0 0 10px 10px;
-    }
-    .index-left-block h2{
-        color: #fff;
-        background: #4fc08d;
-        padding: 10px 15px;
-        margin-bottom: 15px;
-    }
-    .all-products h3{
-        padding: 0 15px 5px 15px;
-        font-weight: bolder;
-        color: #222;
-    }
-    .index-left-block ul{
-        padding: 0 15px 5px 15px;
-    }
-    .hot-tag{
-        color: white;
-        background: purple;
-        font-size: 12px;
-    }
-    .index-board-list{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
-    .index-board-item{
-        width: 400px;
-        background: #fff;
-        box-shadow: 0 0 1px #ddd;
-        margin-bottom: 20px;
-    }
-    .index-board-item-inner{
-        height: 125px;
-        padding-left: 120px;
-        background-image: url(../assets/logo.png);
-        background-repeat: no-repeat;
-        background-size: 20%;
-        background-position: 20px;
-    }
-    .index-board-item-inner h2{
-        font-size: 18px;
-        font-weight: bolder;
-        color: #222;
-        margin-bottom: 15px;
-    }
-    .index-board-item-inner p{
-        margin-bottom: 15px;
-    }
-    .index-board-button{
-        width: 80px;
-        height: 30px;
-        line-height: 30px;
-        color: #fff;
-        background: greenyellow;
-        border-radius: 5px;
-        text-align: center;
-    }
-    
+<style scoped>
+/* 只在当前页面起作用 */
+
+.index-wrapper{
+    width: 1200px;
+    margin: 0 auto;
+    /* 弹性盒子   父元素设置display:flex;*/
+    display:flex;
+}
+
+.index-left{
+    width: 298px;
+    /* border: 1px solid black; */
+}
+.index-right{
+    width: 700px;
+}
+
+.index-left-block{
+    margin: 15px;
+    background: #ffffff;
+    box-shadow: 0 0 1px #ddd;
+    border-radius: 0 0 10px 10px;
+}
+.index-left-block h2{
+    color: #ffffff;
+    background: #4fc084;
+    padding: 10px 15px;
+    margin-bottom: 15px;
+
+}
+.all-products h3{
+    padding: 0 15px 5px 15px;
+    font-weight: bolder;
+    color: #222;
+
+}
+.index-left-block ul{
+    padding: 10px 15px;
+
+}
+.index-left-block li{
+    padding: 10px 15px;
+
+}
+
+.hot-tag{
+    color: white;
+    background: purple;
+    font-size: 12px;
+}
+
+.index-board-list{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+}
+.index-board-item{
+    width: 330px;
+    background: white;
+    box-shadow:  0 0 1 #ddd;
+    margin-bottom: 20px;
+    padding-top:20px;
+}
+
+
+.index-board-item-inner{
+    height: 125px;
+    padding-left: 120px;
+    /* background-image: url(../assets/logo.png); */
+    background-repeat: no-repeat;
+    background-size: 20%;
+    background-position: 30px;
+
+
+}
+.index-board-item-inner h2{
+    font-size: 18px;
+    font-weight: bolder;
+    color: black;
+    margin-bottom: 15px;
+}
+
+.index-board-item-inner p{
+    margin-bottom: 15px;
+
+}
+.index-board-button{
+    width: 80px;
+    height: 30px;
+    color: #fff;
+
+    background: #4fc084;
+    border-radius: 5px;
+    text-align: center;
+    line-height: 30px;
+
+}
+.index-board-item img{
+    position: absolute;
+    width: 100px;
+}
+
 
 </style>
---
+++ day1/simple/src/components/sliderComponent.vue
@@ -0,0 +1,112 @@
+<template>
+<!-- 绑定事件 -->
+    <div class="slider-component" @mouseover="clearInv" @mouseout="runInv">
+        <div v-show='index===nowIndex' v-for="(imgUrl,index) in sliderImgList" :key="index" class="slider-item item1"  v-bind:class="['item'+index]">
+            <a href="">
+                <img v-bind:src=imgUrl alt="">
+            </a>
+        </div>
+
+        <ul class="slider-dots">
+            <li>&lt;</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>&gt;</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            nowIndex:0,
+            sliderImgList:[require('../assets/pic1.png'),require('../assets/pic2.png'),require('../assets/pic3.png'),require('../assets/pic4.png')]
+        }
+    },
+    // 钩子函数、
+    methods: {
+        autoPlay(){
+            this.nowIndex++;
+            if(this.nowIndex===4){
+                this.nowIndex=0
+            }
+        },
+        // 定时切换
+        runInv(){
+            this.invId=setInterval(this.autoPlay,1000)
+            
+        },
+        clearInv(){
+            clearInterval(this.invId)
+           
+        },
+        
+
+    },
+    created() {
+        console.log("haominmin")
+        // 调用函数
+        this.runInv()
+    },
+}
+
+</script>
+
+<style >
+.slider-component{
+    width: 900px;
+    height: 500px;
+    /* 溢出影藏 */
+    overflow: hidden;
+    position: relative;
+}
+
+.slider-item{
+    width: 900px;
+    height: 500px;
+    position: absolute;
+
+}
+.item1{
+    /* 值越大就这上面 */
+    z-index: 100;
+}
+
+.item2{
+    z-index: 90;
+}
+
+.item3{
+    z-index: 80;
+}
+
+.item4{
+    z-index: 70;
+}
+
+.slider-dots{
+    position: absolute;
+    right: 50px;
+    bottom: 20px;
+    z-index: 200;
+    
+}
+.slider-dots li{
+    width: 20px;
+    height: 20px;
+    background: black;
+    /* 横着 */
+    border-radius: 50%;
+    float: left;
+    color: white;
+    text-align: center;
+    opacity: 0.6;
+    margin: 0 auto;
+    
+}
+
+</style>
+
